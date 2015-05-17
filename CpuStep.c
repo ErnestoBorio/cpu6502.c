cbd3fd4e2d926e33d73aee931944d529081f3576
Ernesto Borio <endesmomo@gmail.com>
2015-05-17 17:22:14 -0300

    Oops.

--- CpuStep.c
+++ CpuStep.c
@@ -264,6 +264,7 @@ int Cpu6502_CpuStep( Cpu6502 *cpu )
 // BRK, opcode 00, although it actually takes 7 cycles, has 0 cycles here, because the 7 cycles
 // will be counted inside the IRQ() function, regardless if it came from a BRK or a hardware interrupt.
 
+static const int opcode_cycles[0x100] = {
 // 0 1 2 3 4 5 6 7 8 9 A B C D E F
    0,6,2,8,3,3,5,5,3,2,2,2,4,4,6,6, //00
    2,5,2,8,4,4,6,6,2,4,2,7,4,4,7,7, //10
